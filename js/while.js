628e987ef0306459b7dab35aaf7fa59ddfd117c1
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-06-29 12:26:26 -0500

    Finished do while section of while.js

--- js/while.js
+++ js/while.js
@@ -4,3 +4,16 @@ while(i < 16) {
     x*=2;
     i++;
 }
+
+let allCones = Math.floor(Math.random() * 50) + 50;
+do {
+    let randomCones = Math.floor(Math.random() * 5) + 1;
+    if(randomCones > allCones) {
+        console.log(`Cannot sell you ${randomCones} cones, I only have ${allCones}...`)
+    } else {
+        console.log(`${randomCones} sold...`);
+        allCones -= randomCones;
+    }
+} while(allCones > 0);
+console.log("Yay! I sold them all!");
+
