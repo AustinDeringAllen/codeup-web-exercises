d0e0d1729a30cab48e59e95c1ee285ed6d1ce711
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-07-01 09:24:53 -0500

    Arrays Exercise

--- js/iterating.js
+++ js/iterating.js
@@ -7,11 +7,15 @@
      * 'names'.
      */
 
+    let names = ["Austin","Dylan","Christian","Arthur"];
+
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
+
+    console.log(names.length);
 
     /**
      * TODO:
@@ -19,16 +23,28 @@
      * accessing each element's index.
      */
 
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+    console.log(names[4]); // Undefined
+
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
 
+    for(let i=0; i<names.length; i++) {
+        console.log(names[i]);
+    }
+
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+
+    names.forEach(e => console.log(e));
 
     /**
      * TODO:
@@ -43,5 +59,20 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+
+    const first = function(array) {
+        return array[0];
+    }
+    first(names);
+
+    const second = function(array) {
+        return array[1];
+    }
+    second(names);
+
+    const last = function(array) {
+        return array[array.length-1];
+    }
+    last(names);
 
 })();
