15eb423ced22cb5f52b4077ad4789813f0810309
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-07-27 09:44:07 -0500

    finished functionality entirely

--- js/weather_map.js
+++ js/weather_map.js
@@ -11,18 +11,20 @@ $(document).ready(function() {
     const renderWeather = function(forecasts) {
         let html = "";
         forecasts.forEach(function(forecast) {
+            let icon = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
             let card = `
                 <div class="card" style="width: 15rem;">
                 <div id="weather-date" class="card-header">${forecast.date}</div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item text-center">${forecast.high.toFixed(1)}°F / ${forecast.low.toFixed(1)}°F</li>
+                    <li class="list-group-item text-center"><img src=${icon} alt=""></li>
                     <li class="list-group-item">Weather: ${forecast.weather[0].main}</li>
                     <li class="list-group-item">Humidity: ${forecast.humidity}%</li>
                     <li class="list-group-item">Wind Speed: ${forecast.wind}mph</li>
                     <li class="list-group-item">Pressure: ${forecast.pressure}mmHg</li>
                 </ul>
             </div>    
-            `
+            `;
             html += card;
         });
         $('#card-block').html("").append(html);
@@ -31,6 +33,7 @@ $(document).ready(function() {
     const getWeather = function(lat, lon) {
         let weatherMap = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${weatherMapKey}&units=imperial`;
         $.get(weatherMap).done(function(data) {
+            console.log(data);
             let bucket = [];
             data.daily.forEach((e, i) => {
                 if(i < 5) {
@@ -50,6 +53,17 @@ $(document).ready(function() {
         });
     }
 
+    const markerDrag = function() {
+        let coords = [marker.getLngLat().lng, marker.getLngLat().lat];
+        let mapbox = `https://api.mapbox.com/geocoding/v5/mapbox.places/${coords[0]},${coords[1]}.json?access_token=${mapboxKey}`;
+        getWeather(coords[1],coords[0]);
+        renderLocation(coords);
+        $.get(mapbox).done(function(data) {
+            $('#location').val("");
+            $('#location').attr('placeholder', data.features[0].place_name);
+        });
+    }
+
     const renderLocation = function(coords) {
         map.flyTo({
             center: coords,
@@ -62,7 +76,7 @@ $(document).ready(function() {
         });
         if(marker)
             marker.remove();
-        marker = new mapboxgl.Marker().setLngLat(coords).setDraggable(true).addTo(map).getElement();
+        marker = new mapboxgl.Marker().setLngLat(coords).setDraggable(true).addTo(map).on('dragend', markerDrag);
     }
 
     const getLocation = function(location) {
@@ -74,17 +88,6 @@ $(document).ready(function() {
         });
     }
     getLocation('Northridge, CA');
-
-    map.on('click',function(e) {
-        let coords = [e.lngLat.lng,e.lngLat.lat];
-        let mapbox = `https://api.mapbox.com/geocoding/v5/mapbox.places/${coords[0]},${coords[1]}.json?access_token=${mapboxKey}`;
-        getWeather(coords[1],coords[0]);
-        renderLocation(coords);
-        $.get(mapbox).done(function(data) {
-            $('#location').val("");
-            $('#location').attr('placeholder', data.features[0].place_name);
-        });
-    });
 
     $('#submit').click(function(e) {
         e.preventDefault();
